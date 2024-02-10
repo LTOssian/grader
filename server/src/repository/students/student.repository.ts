2662157f07e90564fa8d924553db7834da7e43c6
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-10 13:25:55 +0100

    feat: add student by id get request

--- server/src/repository/students/student.repository.ts
+++ server/src/repository/students/student.repository.ts
@@ -1,10 +1,28 @@
+import { ErrorMessageEnum } from "../../common/constants";
+import NotFoundError from "../../common/errors/not-found.error";
 import { DbClient } from "../../infrastructure/database/db-client";
 import { Student } from "../../infrastructure/database/interfaces/students-table.type";
 
 class StudentRepository {
-  public async getStudentsFromGroup(groupId: string): Promise<Student[]> {
-    const result = await DbClient.selectFrom("students").selectAll().where("students.group_id", "=", groupId).execute();
-    return result;
+  public async getStudentsFromGroup(credentials: Pick<Student, "group_id">): Promise<Student[]> {
+    const rows = await DbClient.selectFrom("students")
+      .selectAll()
+      .where("group_id", "=", credentials.group_id)
+      .execute();
+    return rows;
+  }
+
+  public async getStudentsByID(credentials: Partial<Pick<Student, "group_id" | "student_id">>) {
+    const baseQuery = await DbClient.selectFrom("students").selectAll();
+
+    if (credentials.group_id) baseQuery.where("group_id", "=", credentials.group_id);
+    if (credentials.student_id) baseQuery.where("student_id", "=", credentials.student_id);
+
+    const rows = baseQuery.executeTakeFirst();
+
+    if (!rows) throw new NotFoundError({ message: ErrorMessageEnum.UNKNOWN_ID, code: 404 });
+
+    return rows;
   }
 }
 
