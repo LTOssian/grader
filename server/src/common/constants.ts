3b2a7d7088685c3ff42e3fd306fdb09bd124c37b
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-10 14:57:50 +0100

    feat: improve error validation by making the rule method for required fields to take in an object and filter missing keys also added constants and revised error response code

--- server/src/common/constants.ts
+++ server/src/common/constants.ts
@@ -1,6 +1,8 @@
 export enum ErrorMessageEnum {
   INVALID_DATA = "Validation error. Check your credentials.",
-  UNKNOWN_ID = "This ID does not match any todo.",
+  UNKNOWN_ID = "This ID does not match.",
+  UNKNOWN_GROUP = "This ID does not match any group.",
+  UNKNOWN_STUDENT = "This ID does not match any student.",
   BAD_REQUEST = "Bad request.",
   FIELD_REQUIRED = "This field is required.",
 }
--- server/src/common/validators/rules.ts
+++ server/src/common/validators/rules.ts
@@ -1,14 +1,23 @@
-import { ErrorMessageEnum } from "../constants";
 import { TValidationResult } from "./validator";
 
 /**
- * Checks that the values exists
- * @param value
- * @returns Object with isValid and list of strings
+ * Checks that all given fields are truthy and non-empty
+ * @param value object with the fields
+ * @returns boolean isValid & list of missingKeys
  */
-export const requiredRule = (value: any): TValidationResult => {
-  if (value === undefined || value === null || value === "") {
-    return { isValid: false, errors: [ErrorMessageEnum.FIELD_REQUIRED] };
+export const requiredCredentialsRule = (credentials: Record<string, any>): TValidationResult => {
+  const missingKeys: string[] = [];
+
+  // Loop over keys
+  for (const key of Object.keys(credentials)) {
+    const value = credentials[key];
+    if (value === undefined || value === null || value === "") {
+      missingKeys.push(key);
+    }
+  }
+
+  if (missingKeys.length > 0) {
+    return { isValid: false, errors: missingKeys };
   }
 
   return { isValid: true, errors: [] };
