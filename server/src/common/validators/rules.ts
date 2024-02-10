7d80df30437ebe858f4f4537edd354524d8be18b
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-10 12:27:48 +0100

    style: rename type variables

--- server/src/common/validators/rules.ts
+++ server/src/common/validators/rules.ts
@@ -1,12 +1,12 @@
 import { ErrorMessageEnum } from "../constants";
-import { ValidationResult } from "./validator";
+import { TValidationResult } from "./validator";
 
 /**
  * CHecks that the values exists
  * @param value
  * @returns Object with isValid and list of strings
  */
-export const requiredRule = (value: any): ValidationResult => {
+export const requiredRule = (value: any): TValidationResult => {
   if (value === undefined || value === null || value === "") {
     return { isValid: false, errors: [ErrorMessageEnum.FIELD_REQUIRED] };
   }
--- server/src/common/validators/validator.ts
+++ server/src/common/validators/validator.ts
@@ -2,21 +2,21 @@ import { NewGroup } from "../../infrastructure/database/interfaces/groups-table.
 import SingletonWrapper from "../helpers/singleton-wrapper";
 import { requiredRule } from "./rules";
 
-export type ValidationResult = {
+export type TValidationResult = {
   isValid: boolean;
   errors: string[];
 };
 
-export type ValidationRule<T> = (value: T) => ValidationResult;
+export type TValidationRule<T> = (value: T) => TValidationResult;
 
 export class CustomValidator<T> {
-  private rules: ValidationRule<T>[] = [];
+  private rules: TValidationRule<T>[] = [];
 
   /**
    * Add a validation rule to validate against
    * @param rule callback method that handles validations
    */
-  public addRule(rule: ValidationRule<T>): void {
+  public addRule(rule: TValidationRule<T>): void {
     this.rules.push(rule);
   }
 
@@ -25,18 +25,18 @@ export class CustomValidator<T> {
    * @param value argument to validate against
    * @returns object with isValid boolean and list of errors
    */
-  public validate(value: T): ValidationResult {
-    const validationResult: ValidationResult = { isValid: true, errors: [] };
+  public validate(value: T): TValidationResult {
+    const TValidationResult: TValidationResult = { isValid: true, errors: [] };
 
     for (const rule of this.rules) {
       const result = rule(value);
       if (!result.isValid) {
-        validationResult.isValid = false;
-        validationResult.errors.push(...result.errors);
+        TValidationResult.isValid = false;
+        TValidationResult.errors.push(...result.errors);
       }
     }
 
-    return validationResult;
+    return TValidationResult;
   }
 }
 
