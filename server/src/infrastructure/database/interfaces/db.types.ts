2fc39528d49e30be8ea6e99a2fbfedb121042cb9
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-11 14:57:52 +0100

    chore: rename studentGrades by simply Grades

--- server/src/infrastructure/database/interfaces/db.types.ts
+++ server/src/infrastructure/database/interfaces/db.types.ts
@@ -1,13 +1,13 @@
 import { Classes, ClassesTable } from "./group-classes.type";
 import { Group, GroupsTable } from "./groups-table.type";
 import { Student, StudentsTable } from "./students-table.type";
-import { StudentGrades, StudentGradesTables } from "./student-grades.type";
+import { Grades, GradesTables } from "./student-grades.type";
 
 export interface Database {
   students: StudentsTable;
   groups: GroupsTable;
-  student_grades: StudentGradesTables;
+  student_grades: GradesTables;
   group_classes: ClassesTable;
 }
 
-export type Selectables = Group | Student | Classes | StudentGrades;
+export type Selectables = Group | Student | Classes | Grades;
