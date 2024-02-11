1461b8540b88960459190b06b1f24381ad6d5c64
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-11 21:37:35 +0100

    feat: add seeding db migrations

---
+++ db_migrations/migrations_2_example_seed.sql
@@ -0,0 +1,66 @@
+with exampleGroupId as (
+    insert into groups (name)
+    values ('Groupe Exemple')
+    returning group_id
+)
+
+insert into group_classes (group_id, name, coefficient)
+select 
+    group_id, 
+    unnest(ARRAY[
+        'CS50 - Introduction informatique',
+        'Concept Backend - API',
+        'Langage - TypeScript LV1',
+        'Langage - C# LV2',
+        'Théâtre - Improvisation',
+        'Structure de données et algorithmes',
+        'Sport - Art du Cirque'
+    ]) as name, 
+    unnest(ARRAY[
+        2,
+        2,
+        4,
+        4,
+        2,
+        8,
+        2
+    ]) as coefficient 
+from 
+    exampleGroupId;
+
+with exampleGroupId as (
+    select group_id
+    from groups
+    where name = 'Groupe Exemple'
+)
+
+insert into students (group_id, firstname, lastname, email)
+select 
+    group_id, 
+    unnest(ARRAY[
+        'Jean',
+        'Louisan',
+        'Sabrina',
+        'Eric',
+        'Benjamin'
+    ]) as firstname, 
+    unnest(ARRAY[
+        'Dupont',
+        'Tchitoula',
+        'Attos',
+        'Azer',
+        'Schinkel'
+    ]) as lastname,
+    unnest(ARRAY[
+        'jd@hetic.eu',
+        'lt@hetic.eu',
+        'sa@hetic.eu',
+        'ea@hetic.eu',
+        'bs@hetic.eu'
+    ]) as email 
+from 
+    exampleGroupId;
+
+select * from students;
+select * from groups;
+select * from group_classes;
