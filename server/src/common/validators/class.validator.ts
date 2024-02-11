6f4da026e94c41b407cb30c76c10f0143aa89264
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-11 19:20:59 +0100

    chore: merge issue

--- server/src/common/validators/class.validator.ts
+++ server/src/common/validators/class.validator.ts
@@ -1,10 +1,6 @@
 import { CustomValidator } from "./interfaces/validator";
 import { NewClasses } from "../../infrastructure/database/interfaces/group-classes.type";
-<<<<<<< HEAD
 import { requiredCredentialsRule } from "./rules/required-credentials.rule";
-=======
-import { requiredCredentialsRule } from "./rules/required.rule";
->>>>>>> 46cddfc9511b27b2e6aa3c6f9404f7ed68b6a7b0
 import SingletonWrapper from "../helpers/singleton-wrapper";
 
 export const classValidatorSingleton = SingletonWrapper.makeSingleton(new CustomValidator<NewClasses>()).getInstance();
