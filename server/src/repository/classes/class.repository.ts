a365ead70d845e4a7fd8c8edf89e2339dc3da1f5
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-11 17:48:05 +0100

    chore: improve docs on class repo's method

--- server/src/repository/classes/class.repository.ts
+++ server/src/repository/classes/class.repository.ts
@@ -1,9 +1,14 @@
+import { Classes, NewClasses } from "../../infrastructure/database/interfaces/group-classes.type";
+import { DbClient } from "../../infrastructure/database/db-client";
 import { ErrorMessageEnum } from "../../common/constants";
 import NotFoundError from "../../common/errors/not-found.error";
-import { DbClient } from "../../infrastructure/database/db-client";
-import { Classes, NewClasses } from "../../infrastructure/database/interfaces/group-classes.type";
 
 class ClassRepository {
+  /**
+   * Gets the classes of the specified group
+   * @param credentials contains the group_id
+   * @returns List of classes
+   */
   public async getClassesFromGroup(credentials: Pick<Classes, "group_id">): Promise<Classes[]> {
     const rows = await DbClient.selectFrom("group_classes")
       .selectAll()
@@ -13,12 +18,21 @@ class ClassRepository {
     return rows;
   }
 
+  /**
+   * Inserts a new class in the class table
+   * @param credentials contains the name and the coeff of the class
+   * @returns The class created
+   */
   public async createClassFromGroup(credentials: NewClasses): Promise<Classes> {
     const [rows] = await DbClient.insertInto("group_classes").values(credentials).returningAll().execute();
 
     return rows;
   }
 
+  /**
+   * Delete the class with its id
+   * @param credentials contains the class id
+   */
   public async deleteClassFromGroup(credentials: Pick<Classes, "class_id">): Promise<void> {
     // check validity of the id
     await this.getClassById(credentials);
@@ -26,6 +40,11 @@ class ClassRepository {
     await DbClient.deleteFrom("group_classes").where("class_id", "=", credentials.class_id).execute();
   }
 
+  /**
+   * Gets the class with its id
+   * @param credentials contains the class id
+   * @returns The class
+   */
   public async getClassById(credentials: Pick<Classes, "class_id">): Promise<Classes> {
     const rows = await DbClient.selectFrom("group_classes")
       .selectAll()
