4848d16c0a2b7b2d9d458ac39d33ebbc563f9156
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-11 19:20:59 +0100

    chore: merge issue

--- server/src/common/validators/grade.validator.ts
+++ server/src/common/validators/grade.validator.ts
@@ -1,10 +1,6 @@
 import { CustomValidator } from "./interfaces/validator";
 import { NewGrades } from "../../infrastructure/database/interfaces/student-grades.type";
-<<<<<<< HEAD
 import { requiredCredentialsRule } from "./rules/required-credentials.rule";
-=======
-import { requiredCredentialsRule } from "./rules/required.rule";
->>>>>>> 46cddfc9511b27b2e6aa3c6f9404f7ed68b6a7b0
 import { validReportRule } from "./rules/valid-grade.rule";
 import SingletonWrapper from "../helpers/singleton-wrapper";
 
--- server/src/common/validators/group.validator.ts
+++ server/src/common/validators/group.validator.ts
@@ -1,10 +1,6 @@
 import { CustomValidator } from "./interfaces/validator";
 import { NewGroup } from "../../infrastructure/database/interfaces/groups-table.type";
-<<<<<<< HEAD
 import { requiredCredentialsRule } from "./rules/required-credentials.rule";
-=======
-import { requiredCredentialsRule } from "./rules/required.rule";
->>>>>>> 46cddfc9511b27b2e6aa3c6f9404f7ed68b6a7b0
 import SingletonWrapper from "../helpers/singleton-wrapper";
 
 export const groupValidatorSingleton = SingletonWrapper.makeSingleton(new CustomValidator<NewGroup>()).getInstance();
