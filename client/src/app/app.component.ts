acb3e677fc95ae12ab1710563037bddb6b17e0fe
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-12 22:59:18 +0100

    feat: add home comp and base styles

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SidebarGroupComponent } from './sidebar-group/sidebar-group.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SidebarGroupComponent],
 })
 export class AppComponent {
   title = 'client';
