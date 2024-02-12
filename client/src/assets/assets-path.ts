3d02a0bbe993338b07eb4d1b6c0d45998690ec48
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-12 22:50:40 +0100

    feat: add some svgsassets folder

---
+++ client/src/assets/assets-path.ts
@@ -0,0 +1,6 @@
+export const enum AssetPath {
+  TRASH = '/assets/svg/trash.svg',
+  ARROW_DOWN = '/assets/svg/arrow-down.svg',
+  ARROW_UP = '/assets/svg/arrow-up.svg',
+  LOGOTYPE = '/assets/svg/logotype.svg',
+}
