1d3905e6dc3b680bcd7353bc284ebafcb86ccbd9
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-13 22:26:12 +0100

    feat: use subjects to trigger fetch group event on different event when user deletes a group and on init

--- client/src/app/sidebar-group/sidebar-group.component.ts
+++ client/src/app/sidebar-group/sidebar-group.component.ts
@@ -1,26 +1,31 @@
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { JsonPipe } from '@angular/common';
-import { Router } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
+import { Subject, switchMap } from 'rxjs';
+
+import { AssetPath } from '../../assets/assets-path';
 import { GroupModel } from '../interfaces/group.model';
 import { GroupService } from '../services/group.service';
-import { AssetPath } from '../../assets/assets-path';
 
 @Component({
   selector: 'app-sidebar-group',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, RouterLink],
   templateUrl: './sidebar-group.component.html',
   styleUrl: './sidebar-group.component.scss',
 })
 export class SidebarGroupComponent {
   private groupService = inject(GroupService);
   private destroyRef = inject(DestroyRef);
+  private activatedRoute = inject(ActivatedRoute);
   private router = inject(Router);
 
   public groups = signal<GroupModel[]>([]);
   public groupListIsVisible = true;
+
+  private getGroupsTrigger$ = new Subject<void>();
 
   public assetsStore = {
     trashSvg: {
@@ -41,18 +46,44 @@ export class SidebarGroupComponent {
     },
   };
 
-  constructor() {
-    this.groupService
-      .getAllGroups()
-      .pipe(takeUntilDestroyed(this.destroyRef))
+  public constructor() {
+    // Subject to trigger the get group method
+    this.getGroupsTrigger$
+      .pipe(
+        switchMap(() => this.groupService.getAllGroups()),
+        takeUntilDestroyed(this.destroyRef)
+      )
       .subscribe((groupData) => this.groups.set(groupData.data));
+
+    // Initialize the subjects
+    this.getGroupsTrigger$.next();
   }
 
-  navigateToGroup({ group_id }: Pick<GroupModel, 'group_id'>) {
-    return this.router.navigateByUrl(`/group/${group_id}`);
+  /**
+   * Navigates to the group's page
+   * @param param0 group_id key holding the group id
+   */
+  public async navigateToGroup({ group_id }: Pick<GroupModel, 'group_id'>) {
+    await this.router.navigateByUrl(`/group/${group_id}`);
   }
 
-  navigateToHome() {
+  /**
+   * Navigates to the home page
+   */
+  public navigateToHome() {
     return this.router.navigateByUrl('');
+  }
+
+  /**
+   * Removes the group
+   * @param param0 group_id key holding the group id
+   */
+  public removeGroupFromList({ group_id }: Pick<GroupModel, 'group_id'>) {
+    this.groupService
+      .deleteGroupById(group_id)
+      .subscribe(() => {
+        this.getGroupsTrigger$.next();
+      })
+      .add(() => this.navigateToHome());
   }
 }
