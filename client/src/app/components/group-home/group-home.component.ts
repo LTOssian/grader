f2ad3ccebf10a134d9dc8f3fc452b8e0b4fd3c0b
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-14 20:26:03 +0100

    feat: add subject and subscriptions for group and future student services made sure to handle lifeCYcle of subscriptions otherwise there were more and more requests each load

--- client/src/app/components/group-home/group-home.component.ts
+++ client/src/app/components/group-home/group-home.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, Input, inject, signal } from '@angular/core';
+
+import { Subject, Subscription, switchMap } from 'rxjs';
+
+import { ClassModel } from '../../interfaces/class.model';
+import { ClassService } from '../../services/class.service';
+import { GroupModel } from '../../interfaces/group.model';
+import { StudentModel } from '../../interfaces/student.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-group-home',
   standalone: true,
   imports: [],
   templateUrl: './group-home.component.html',
-  styleUrl: './group-home.component.scss'
+  styleUrl: './group-home.component.scss',
 })
 export class GroupHomeComponent {
+  @Input()
+  set group_id(group_id: string) {
+    this.initializeTriggers({ group_id });
+  }
 
+  private classService = inject(ClassService);
+  private destroyRef = inject(DestroyRef);
+
+  // Signals with business data
+  public classes = signal<ClassModel[]>([]);
+  public students = signal<StudentModel[]>([]);
+
+  // Subjects triggers
+  private getClassesTrigger$ = new Subject<void>();
+  private getStudentsTrigger$ = new Subject<void>();
+
+  // Main subscription
+  private triggersSubscription = new Subscription();
+
+  private initializeTriggers({ group_id }: Pick<GroupModel, 'group_id'>) {
+    this.resetSubscription();
+
+    // Set up subscriptions
+    const classesSubscription = this.getClassesTrigger$
+      .pipe(
+        switchMap(() =>
+          this.classService.getAllStudentsFromGroup({
+            group_id: group_id,
+          })
+        ),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((classData) => this.classes.set(classData.data));
+
+    const studentSubscription = this.getStudentsTrigger$.subscribe(); // TODO
+
+    // Add subscriptions to main stream
+    this.triggersSubscription.add(classesSubscription);
+    this.triggersSubscription.add(studentSubscription);
+
+    // Initialize first call
+    this.getClassesTrigger$.next();
+    this.getStudentsTrigger$.next();
+  }
+
+  private resetSubscription() {
+    this.triggersSubscription.unsubscribe();
+    this.triggersSubscription = new Subscription();
+  }
 }
