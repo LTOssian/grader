0a8bcae3b80367151444193295c3bdf5f5e73d3f
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-14 03:32:38 +0100

    feat: add submit, cancel and error handling for reusable modal

--- client/src/app/components/modal-create-form/modal-create-form.component.ts
+++ client/src/app/components/modal-create-form/modal-create-form.component.ts
@@ -1,43 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JsonPipe, KeyValuePipe } from '@angular/common';
 
-import { FormModel } from '../../interfaces/form-model';
+import { ErrorModel } from '../../interfaces/error-model';
+import { FormType } from '../../interfaces/form-model';
+import { GroupService } from '../../services/group.service';
 import { MdbModalModule, MdbModalRef } from 'mdb-angular-ui-kit/modal';
+import { ModalFormButtonComponent } from './modal-form-button/modal-form-button.component';
 
 @Component({
   selector: 'app-modal-create-form',
   standalone: true,
+  templateUrl: './modal-create-form.component.html',
+  styleUrl: './modal-create-form.component.scss',
   imports: [
     FormsModule,
     JsonPipe,
     KeyValuePipe,
     MdbModalModule,
+    ModalFormButtonComponent,
     ReactiveFormsModule,
   ],
-  templateUrl: './modal-create-form.component.html',
-  styleUrl: './modal-create-form.component.scss',
 })
 export class ModalCreateFormComponent {
+  public modalRef = inject(MdbModalRef<ModalCreateFormComponent>);
+  private groupService = inject(GroupService);
+
+  // Modal bound properties
   public title: string | null = null;
-  public inputs: FormModel[] | null = null;
+  public labelsByInput: Record<string, string> | null = null;
+  public entityToCreate: FormType | null = null;
   public modalFormGroup: FormGroup | null = null;
 
-  constructor(public modalRef: MdbModalRef<ModalCreateFormComponent>) {}
+  public formState?: ErrorModel;
 
-  public getTypeOf(value: any) {
+  /**
+   * Gets the type of the var
+   * @param value var with unknown value
+   * @returns type as string
+   */
+  public getTypeOf(value: any): string {
     return typeof value;
   }
 
-  public getFormControlKey(controlField: unknown): string {
-    return controlField as string;
+  /**
+   * Infers type for the formControlKey in the template
+   * @param controlField
+   * @returns param as string
+   */
+  public getFormControlKey(controlFieldKey: unknown): string {
+    return controlFieldKey as string;
   }
 
+  /**
+   * Gets the value of the label via the labelsByInputs record
+   * @param keyValue form control key
+   * @returns the label of the specified input
+   */
   public getLabelNameFromInputs(keyValue: unknown): string {
-    return (
-      this.inputs?.find((input) =>
-        Object.keys(input).some((key) => key == keyValue)
-      )?.name || ''
-    );
+    return this.labelsByInput?.[keyValue as string] || '';
+  }
+
+  /**
+   * Perfmors request bases on the type of the current modal
+   */
+  public onSubmit(): void {
+    if (!this.entityToCreate) this.cancelModal();
+    if (!this.modalFormGroup) return;
+
+    switch (this.entityToCreate) {
+      case 'group':
+        this.groupService
+          .createGroupWithBody(this.modalFormGroup.value)
+          .subscribe({
+            next: (responseData) => {
+              this.modalRef.close({
+                message: 'Entitée crée avec succès',
+                isSuccess: true,
+                entity: responseData.data,
+              });
+            },
+            error: (responseError) => {
+              this.formState = responseError.error;
+            },
+          })
+          .add(() => {
+            if (this.formState) {
+              setTimeout(() => (this.formState = undefined), 8000);
+            }
+          });
+        break;
+    }
+  }
+
+  /**
+   * Closes the modal with a fail message
+   */
+  public cancelModal(): void {
+    this.modalRef.close({
+      message: 'Opération annulée',
+      isSuccess: false,
+    });
   }
 }
