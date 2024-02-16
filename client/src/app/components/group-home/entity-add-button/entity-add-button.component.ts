b308cfef774d4348e896d0837bd0bbc14ebaa905
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-16 22:42:40 +0100

    feat: button for adding entities

--- client/src/app/components/group-home/entity-add-button/entity-add-button.component.ts
+++ client/src/app/components/group-home/entity-add-button/entity-add-button.component.ts
@@ -1,10 +1,15 @@
 import { Component, input } from '@angular/core';
 
+import { AssetPath } from '../../../../assets/assets-path';
+
 @Component({
   selector: 'app-entity-add-button',
   standalone: true,
   imports: [],
-  template: '<button type="button">{{ buttonAddText() }}</button>',
+  template: `<button type="button">
+    {{ buttonAddText() }}
+    <img [src]="assetsStore.plusSvg.path" [alt]="assetsStore.plusSvg.path" />
+  </button>`,
   styles: `
     button {
       background-color: var(--clr-black-primary);
@@ -16,9 +21,20 @@ import { Component, input } from '@angular/core';
       color: var(--clr-white-primary);
       font-weight: 500;
       font-size: var(--fs-body);
+
+      display: flex;
+      align-items: center;
+      gap: 16px;
     }
   `,
 })
 export class EntityAddButtonComponent {
   public buttonAddText = input.required<string>();
+
+  public assetsStore = {
+    plusSvg: {
+      path: AssetPath.PLUS,
+      alt: 'Icône ajout',
+    },
+  };
 }
