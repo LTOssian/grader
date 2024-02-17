4038cf7f5c7d04a8fbea42ae04aac13230a7c348
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-17 20:30:16 +0100

    feat: implement modal to download pdfs

--- client/src/app/components/modal-create-form/modal-create-form.component.ts
+++ client/src/app/components/modal-create-form/modal-create-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpParams } from '@angular/common/http';
 import { JsonPipe, KeyValuePipe } from '@angular/common';
 
 import { MdbModalModule, MdbModalRef } from 'mdb-angular-ui-kit/modal';
@@ -9,6 +10,7 @@ import { ClassModel } from '../../interfaces/class.model';
 import { ClassService } from '../../services/class.service';
 import { ErrorModel } from '../../interfaces/error.model';
 import { FormType, ModalMessageEnum } from '../../interfaces/form.model';
+import { GradeService } from '../../services/grade.service';
 import { GroupModel } from '../../interfaces/group.model';
 import { GroupService } from '../../services/group.service';
 import { ModalFormButtonComponent } from './modal-form-button/modal-form-button.component';
@@ -33,15 +35,18 @@ export class ModalCreateFormComponent {
   public modalRef = inject(MdbModalRef<ModalCreateFormComponent>);
   private classService = inject(ClassService);
   private groupService = inject(GroupService);
+  private gradeService = inject(GradeService);
   private studentService = inject(StudentService);
 
   // Modal bound properties
   public title: string | null = null;
+  public subtitle: string | null = null;
   public labelsByInput: Record<string, string> | null = null;
   public placeholdersByInput: Record<string, string | number> | null = null;
   public entityToCreate: FormType | null = null;
   public modalFormGroup: FormGroup | null = null;
   public group_id: string | null = null;
+  public entity_id: string | null = null;
 
   public formState?: ErrorModel;
 
@@ -87,7 +92,7 @@ export class ModalCreateFormComponent {
   public onSubmit(): void {
     if (!this.entityToCreate) this.cancelModal();
     if (!this.modalFormGroup) return;
-    console.log(this.modalFormGroup.value);
+
     switch (this.entityToCreate) {
       case 'group':
         this.executeEntityCreation(
@@ -142,6 +147,45 @@ export class ModalCreateFormComponent {
             message: ModalMessageEnum,
             isSuccess: true,
             entity: responseData.data,
+          });
+        },
+        error: (responseError) => {
+          this.formState = responseError.error;
+        },
+      })
+      .add(() => {
+        if (this.formState) {
+          setTimeout(() => (this.formState = undefined), 8000);
+        }
+      });
+  }
+
+  public downloadPdf(version: 'simple' | 'complete') {
+    this.gradeService
+      .downloadGradeById({
+        student_grades_id: this.entity_id || '',
+        params: new HttpParams().set('version', version),
+      })
+      .subscribe({
+        next: (responseData) => {
+          const virtualUrl = window.URL.createObjectURL(
+            responseData.body as Blob
+          );
+          const virtualLink = document.createElement('a');
+          virtualLink.href = virtualUrl;
+          virtualLink.download =
+            responseData.filename?.split('="')[1] ||
+            `Bulletin_${version}_${this.entity_id}`;
+
+          virtualLink.click();
+
+          // Clean up
+          window.URL.revokeObjectURL(virtualUrl);
+
+          this.modalRef.close({
+            message: ModalMessageEnum,
+            isSuccess: true,
+            entity: responseData.filename,
           });
         },
         error: (responseError) => {
