4f56b918b6274593c3c7da869d25cc7f27efc5e5
Louisan Tchitoula <t.louisan@outlook.fr>
2024-02-17 22:28:05 +0100

    feat: add new form page component and route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { GroupHomeComponent } from './components/group-home/group-home.component';
 import { EmptyComponent } from './components/empty/empty.component';
 import { GradesPageComponent } from './components/grades-page/grades-page.component';
+import { GradeCreateFormComponent } from './components/grade-create-form/grade-create-form.component';
 
 export const routes: Routes = [
   {
@@ -16,5 +17,9 @@ export const routes: Routes = [
   {
     path: 'grades/:group_id_param',
     component: GradesPageComponent,
+  },
+  {
+    path: 'student/:student_id',
+    component: GradeCreateFormComponent,
   },
 ];
---
+++ client/src/app/components/grade-create-form/grade-create-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-create-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './grade-create-form.component.html',
+  styleUrl: './grade-create-form.component.scss'
+})
+export class GradeCreateFormComponent {
+
+}
